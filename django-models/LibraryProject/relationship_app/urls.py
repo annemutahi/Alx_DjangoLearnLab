9f8db9781f6c940a50860186f347921ee0afecfe
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-07 15:04:41 +0300

    made changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import list_books, LibraryDetailView, register
+from .views import list_books, LibraryDetailView
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name ='library-detail'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
 ]
