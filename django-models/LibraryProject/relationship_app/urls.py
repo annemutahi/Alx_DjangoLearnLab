de26d3fb0072477aec183b29e4337eb92bf5acc8
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-07 15:02:10 +0300

    made changes

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('books/', list_books, name='book-list'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name ='library-detail'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('register/', register, name='register'),
 ]
