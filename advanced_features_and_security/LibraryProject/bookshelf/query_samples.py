4b7bf580f58dfd1207cdd72f50d75188009abe29
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-14 11:51:27 +0300

    renamed app to bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/query_samples.py
+++ advanced_features_and_security/LibraryProject/bookshelf/query_samples.py
@@ -1,4 +1,4 @@
-from relationship_app.models import Author, Book, Library, Librarian
+from bookshelf.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
