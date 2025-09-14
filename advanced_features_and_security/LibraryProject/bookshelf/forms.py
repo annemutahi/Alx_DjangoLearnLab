00305acb5b8aec3b810ed16d0ca9857946ad60cc
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-14 20:23:19 +0300

    added exampleform

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,3 +5,16 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = '__all__'
+
+# Example form to handle Book input safely
+class ExampleForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'published_date']
+
+    # Optional extra validation
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if "<script>" in title.lower():
+            raise forms.ValidationError("Invalid input detected.")
+        return title
