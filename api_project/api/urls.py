afcfa4c851b8c7725c4c6edbec3deae0ec79f4c2
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-21 16:26:14 +0300

    created api view and serializer

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from .views import book_list
+from .views import book_list, BookList
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    path('books/', book_list, name='book-list'),
+    # path('books/', book_list, name='booklist'),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Book
+from rest_framework import generics
+from .serializers import BookSerializer
 
 def book_list(request):
     books = Book.objects.all()
     return render(request, 'api/book_list.html', {'books': books})
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
