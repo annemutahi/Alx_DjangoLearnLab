d1fe4c3d90ad52235302a816315f4a8ed95238a9
annemutahi <ms.nyamburamutahi@gmail.com>
2025-09-28 17:35:20 +0300

    made changes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -24,6 +24,7 @@
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework as filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
