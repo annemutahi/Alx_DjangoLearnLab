a6d0fdb1ff44ef81e1d802d61808db29cd55813f
annemutahi <ms.nyamburamutahi@gmail.com>
2025-10-03 12:30:58 +0300

    added commentcreateview class

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -34,21 +34,6 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = "blog/post_detail.html"
-    @login_required
-    def add_comment(request, pk):
-        post = get_object_or_404(Post, pk=pk)
-        if request.method == "POST":
-            form = CommentForm(request.POST)
-            if form.is_valid():
-                comment = form.save(commit=False)
-                comment.post = post
-                comment.author = request.user
-                comment.save()
-                return redirect('post_detail', pk=post.pk)
-        else:
-            form = CommentForm()
-        return redirect('post_detail', pk=post.pk)  # fallback
-
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
@@ -79,7 +64,19 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         post = self.get_object()
         return self.request.user == post.author
     
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = "blog/comment_form.html"
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('post_detail', kwargs={'pk': self.object.post.pk})
+    
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
     form_class = CommentForm
