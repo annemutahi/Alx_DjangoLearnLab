f103be23bd1fa22355f1ea2a9b947cb6bb8e0a2d
annemutahi <ms.nyamburamutahi@gmail.com>
2025-10-10 23:13:52 +0300

    made changes

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('me/', ProfileView.as_view(), name='profile'),
-    path('users/<int:pk>/follow/', UserViewSet.as_view({'post': 'follow_user'}), name='follow-user'),
-    path('users/<int:pk>/unfollow/', UserViewSet.as_view({'post': 'unfollow_user'}), name='unfollow-user'),
+    path('users/<int:pk>/follow/<int:user_id>/', UserViewSet.as_view({'post': 'follow_user'}), name='follow-user'),
+    path('users/<int:pk>/unfollow/<int:user_id>/', UserViewSet.as_view({'post': 'unfollow_user'}), name='unfollow-user'),
 ]
